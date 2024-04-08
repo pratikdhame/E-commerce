15bef98a7e6a74ec2cd1ad72d27af7af5c308cfd
Pratik Dhame <96607747+pratiktechie@users.noreply.github.com>
2024-04-09 00:36:41 +0530

    User Login API Written 7:47H

--- backend/index.js
+++ backend/index.js
@@ -174,6 +174,27 @@ app.post('/signup',async (req, res) =>{
     res.json({success:true, token})
 })
 
+//Creating an Endpoint for user Login
+
+app.post('/login', async(req, res)=>{
+    let user = await Users.findOne({email:req.body.email});
+    if(user) {
+        const passCompare = req.body.password === user.password;
+        if(passCompare){
+            const data= {
+                user: {
+                    id:user.id
+                }
+            }
+            const token = jwt.sign(data, 'secret_ecom');
+            res.json({success:true, token});
+        }else{
+            res.json({success:false, errors: "Wrong Password"});
+        }
+    }else{
+        res.json({success:false, errors: "Wrong Email Id"});
+    }
+})
 
 app.listen(port, (error)=>{
     if(!error){
