5b52075ace66af70ad1f158fa059ef23359b9705
Pratik Dhame <96607747+pratiktechie@users.noreply.github.com>
2024-04-11 13:28:18 +0530

    fixed bugs

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,12 @@
-const port = 4000;
+const port = process.env.PORT || 4000 ;
 const express = require("express");
 export const app = express();
 const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken")
-const multer = require("multer");
-const path = require("path");
+// const multer = require("multer");
+// const path = require("path");
 const cors = require("cors");
-const { type } = require("os");
+// const { type } = require("os");
 
 app.use(express.json());
 app.use(cors({
@@ -23,24 +23,24 @@ app.get("/",(req, res)=>{
 
 
 //Image Storage Engine
-const storage = multer.diskStorage({
-    destination: './upload/images',
-    filename:(req, file, cb) =>{
-        return cb(null,`${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
-    }
-})
+// const storage = multer.diskStorage({
+//     destination: './upload/images',
+//     filename:(req, file, cb) =>{
+//         return cb(null,`${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
+//     }
+// })
 
-const upload = multer({storage: storage})
+// const upload = multer({storage: storage})
 
 //Creating Upload Endpoint for images
-app.use('/images', express.static('upload/images'))
+// app.use('/images', express.static('upload/images'))
 
-app.post("/upload",upload.single('product'), (req, res)=>{
-    res.json({
-        success: 1,
-        image_url:`http://localhost:${port}/images/${req.file.filename}`
-    })
-})
+// app.post("/upload",upload.single('product'), (req, res)=>{
+//     res.json({
+//         success: 1,
+//         image_url:`http://localhost:${port}/images/${req.file.filename}`
+//     })
+// })
 
 // Schema for Creating Products
 
